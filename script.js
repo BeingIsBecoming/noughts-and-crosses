f1ca5d7df09207a6bd5673b799552111d3af9d44
beingisbecoming <129126782+BeingIsBecoming@users.noreply.github.com>
2025-06-28 20:58:59 -0700

    Add console and HTML board displays for testing.

--- script.js
+++ script.js
@@ -36,6 +36,20 @@ function gameEnd(board) {
 
 const result = gameEnd(gameBoard.getBoard());
 
+// Display Controller
+function printBoard() {
+  const cell = gameBoard.getBoard();
+  console.log(`
+  ${cell[0]} | ${cell[1]} | ${cell[2]}
+  ---------
+  ${cell[3]} | ${cell[4]} | ${cell[5]}
+  ---------
+  ${cell[6]} | ${cell[7]} | ${cell[8]}
+  `);
+}
+
+printBoard();
+
 // Winning Player
 const winner = result === player1.marker ? 
 player1.name : player2.name;
